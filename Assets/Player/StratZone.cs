67bee04e27a52dc5522698be4d2fcd8c12332483
jibin0518 <jibin0518@naver.com>
2023-03-01 22:31:35 +0900

    23.3.1
    까먹음

---
+++ Assets/Player/StratZone.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StratZone : MonoBehaviour
+{
+    public GameManager manger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("!!");
+            manger.StageStart();
+        }
+    }
+}
